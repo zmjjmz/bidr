9cbc614f45613eb193e747d9fda6e720c2a7dc78
Brendan E Ashby <ashbyb@rpi.edu>
2013-11-08 19:46:58 -0500

    pushing skeleton code

---
+++ bidr.py
@@ -0,0 +1,35 @@
+# -*- coding: utf_8 -*-
+
+'''
+@created: Nov 08 2013
+@author: Brendan Ashby & Zach Jablons
+@summary: bidr is a bidding system utilizing social networks
+@copyright: Copyright 2013 - All Rights Reserved
+@license: GPLv3? No Idea
+'''
+
+# Python - Stdlib Imports
+import sys
+import time
+import os
+import logging
+import argparse
+
+# Initiate Logging Globals
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('BIDRLOG')
+
+# Imports - Dependancies
+try:
+	from flask import flask
+	app = Flask(__name__)
+except ImportError:
+	log.error("Failed to import Flask Library. Install it: http://flask.pocoo.org/")
+	sys.exit()
+
+@app.route("/")
+def hello():
+    return "Hello YHACK!"
+
+if __name__ == "__main__":
+    app.run()
